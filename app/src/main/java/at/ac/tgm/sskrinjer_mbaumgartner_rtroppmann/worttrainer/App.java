28970a6c934227716d1ea31e3378da1cdc77bd7b
Lithax <Lithax@outlook.de>
2025-12-04 09:50:07 +0100

    Initial Commit

---
+++ app/src/main/java/at/ac/tgm/sskrinjer_mbaumgartner_rtroppmann/worttrainer/App.java
@@ -0,0 +1,11 @@
+package at.ac.tgm.sskrinjer_mbaumgartner_rtroppmann.worttrainer;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
