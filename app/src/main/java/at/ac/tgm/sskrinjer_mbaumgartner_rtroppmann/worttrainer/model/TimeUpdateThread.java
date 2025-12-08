311c766159160eb9db8df128ced30369bc152b32
Lithax <Lithax@outlook.de>
2025-12-08 19:34:10 +0100

    Drastically improved TimeUpdateThread's real time accuracy

--- app/src/main/java/at/ac/tgm/sskrinjer_mbaumgartner_rtroppmann/worttrainer/model/TimeUpdateThread.java
+++ app/src/main/java/at/ac/tgm/sskrinjer_mbaumgartner_rtroppmann/worttrainer/model/TimeUpdateThread.java
@@ -2,27 +2,33 @@ package at.ac.tgm.sskrinjer_mbaumgartner_rtroppmann.worttrainer.model;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author mhbau
  * @version 1.0
  * @created 08-Dez-2025 15:09:39
  */
-public class TimeUpdateThread extends Thread {
-	public static final int UPDATE_INTERVAL_MS = 1000 * 60; // 1 min
-	private TimeListener timerCallback;
+public class TimeUpdateThread {
 
-	public void setTimeListener(TimeListener l) {
-		this.timerCallback = l;
-	}
+    public static final int UPDATE_INTERVAL_MIN = 1;
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    private TimeListener timerCallback;
 
-	@Override
-	public void run(){
-		while(isAlive() && !isInterrupted()) {
-			try {
-				Thread.sleep(UPDATE_INTERVAL_MS);
-			} catch (Exception e) {}
-			timerCallback.onUpdateTime(LocalDate.now(), LocalTime.now());
-		}
-	}
+    public void setTimeListener(TimeListener l) {
+        this.timerCallback = l;
+    }
+
+    public void start() {
+        scheduler.scheduleAtFixedRate(() -> {
+            if (timerCallback != null) 
+                timerCallback.onUpdateTime(LocalDate.now(), LocalTime.now());
+        }, 0, UPDATE_INTERVAL_MIN, TimeUnit.MINUTES);
+    }
+
+    public void stop() {
+        scheduler.shutdownNow();
+    }
 }//end TimeUpdateThread
