8ab9eaa6e9971e43ca28e13ac3fe4f7f9f4746fd
Grenadier7 <sedrick.skr@gmail.com>
2025-12-22 14:02:32 +0100

    Nix ist fertig - ich hasse mein Leben :(

---
+++ app/src/main/java/at/ac/tgm/sskrinjer_mbaumgartner_rtroppmann/worttrainer/model/woerter/InvalidWortException.java
@@ -0,0 +1,11 @@
+package at.ac.tgm.sskrinjer_mbaumgartner_rtroppmann.worttrainer.model.woerter;
+
+/**
+ * Signalisiert, dass ein Eintrag im JSON logisch ungültig ist
+ * (z.B. fehlender Text, Nomen ohne Artikel, unbekannte Wortart).
+ */
+public class InvalidWortException extends Exception {
+    public InvalidWortException(String message) {
+        super(message);
+    }
+}
